ff395021e4f5f37d607670ab9c6ce6d40e7b2366
somusindhoor <somusindhoor1234@gmail.com>
2023-05-30 16:19:49 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import Navbar from "./Components/navbar"
 function App() {
   return (
     <div className="App">
-      <h1>YouTube</h1>
+      <Navbar />
     </div>
   );
 }
